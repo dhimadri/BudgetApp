6aadfc1a5689549df1a5e1823f9a52707fe7c19a
Deepita <dhimadri57@gmail.com>
2026-08-06 06:47:36 +0600

    Add HomeScreen with pixel font, styling and hover effects

---
+++ src/main/java/com/budgetapp/HomeScreen.java
@@ -0,0 +1,34 @@
+package com.budgetapp;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+
+public class HomeScreen {
+
+    public Scene getScene(Stage stage) {
+        Font pixel = Font.loadFont("file:///D:/projects/BudgetApp/src/main/resources/Press_Start_2P/PressStart2P-Regular.ttf", 14);
+        System.out.println("Font loaded: " + pixel);
+        Label title = new Label("MONEYFESTING");
+        Button btn1 = new Button("Financial Overview");
+        Button btn2 = new Button("Transactions");
+        Button btn3 = new Button("Catagories");
+        Button btn4 = new Button("Goals");
+
+        btn1.setFont(pixel);
+        btn2.setFont(pixel);
+        btn3.setFont(pixel);
+        btn4.setFont(pixel);
+        title.setFont(Font.loadFont(getClass().getResourceAsStream("/fonts/PressStart2P-Regular.ttf"), 24));
+
+        VBox vbox = new VBox(20);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.getChildren().addAll(title,btn1, btn2, btn3, btn4);
+        Scene scene=new Scene(vbox, 900, 600);
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+        return scene;
+    }
+}
--- src/main/java/com/budgetapp/MainApp.java
+++ src/main/java/com/budgetapp/MainApp.java
@@ -3,10 +3,10 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 public class MainApp extends Application{
     @Override
-    public void start(Stage stage){
-        stage.setTitle("Budget App");
-        stage.setWidth(900);
-        stage.setHeight(600);
+    public void start(Stage stage) {
+        HomeScreen homeScreen = new HomeScreen();
+        stage.setScene(homeScreen.getScene(stage));
+        stage.setTitle("Moneyfesting");
         stage.show();
     }
     public static void main(String[]args){
